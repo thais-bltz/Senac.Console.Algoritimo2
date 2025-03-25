583ccf13bf08b32ad07eb24e999b5e722f4d7cd3
Thais Bublitz <thaisbublitz10@gmail.com>
2025-03-25 20:36:29 -0300

    Criação das variaveis

--- Algoritimo2/Algoritimo2/Program.cs
+++ Algoritimo2/Algoritimo2/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int idade = 10;
+string nomeCliente = "Thais Bublitz";
+decimal preco = 20M;
+char letra = 'a';
+var nomeCompleto = "Thais Camila Bublitz";
+var altura = 180;
