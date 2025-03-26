0f0a23deae6c4824482b0788c7f28937b18d5726
Thais Bublitz <thaisbublitz10@gmail.com>
2025-03-25 21:50:34 -0300

    Leitura de Variaveis

--- Algoritimo2/Algoritimo2/Program.cs
+++ Algoritimo2/Algoritimo2/Program.cs
@@ -1,6 +1,19 @@
-﻿int idade = 10;
+﻿int idade = 19;
 string nomeCliente = "Thais Bublitz";
 decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Thais Camila Bublitz";
 var altura = 180;
+Console.WriteLine("Digite seu nome:");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite sua idade: ");
+var Linha = Console.ReadLine();
+idade = int.Parse(Linha);
+Console.WriteLine("idade informada" + idade);
+if (idade < 18)
+    Console.WriteLine("Você não pode estar aqui!");
+
+Console.ReadKey();
